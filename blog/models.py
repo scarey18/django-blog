70e8ed5ea3832bda0b672adca0ec3353a425b44b
Sean Carey <seancarey18@gmail.com>
2018-05-20 16:10:34 -0400

    Made slight tweak to fix comment timestamps

--- blog/models.py
+++ blog/models.py
@@ -14,7 +14,7 @@ class Comment(models.Model):
 	author = models.CharField(max_length=30)
 	body = models.TextField()
 	article = models.ForeignKey(Article, on_delete=models.CASCADE)
-	posted_at = models.DateTimeField(default=timezone.now())
+	posted_at = models.DateTimeField(auto_now_add=True)
 
 	def __str__(self):
 		return self.author
